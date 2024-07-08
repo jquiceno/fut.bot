0d147914e99cb23488be0bf0eb5fb19ca1b941a0
jquiceno <soyjquiceno@gmail.com>
2024-07-08 21:35:16 +0200

    AppController updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { env } from 'node:process';
 
 @Controller('/')
 export class AppController {
   @Get()
   async getAppInfo(): Promise<any> {
     return {
-      appName: 'fut.bot',
-      version: '0.0.1',
+      appName: env.npm_package_name,
+      version: env.npm_package_version,
     };
   }
 }
