c1f9f7e510bfc12d0395da0265d321818b5767a5
jquiceno <soyjquiceno@gmail.com>
2024-07-10 12:27:25 +0200

    app controller updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { env } from 'node:process';
+import * as pkg from '../package.json';
 
 @Controller('/')
 export class AppController {
   @Get()
   async getAppInfo(): Promise<any> {
     return {
-      appName: env.npm_package_name,
-      version: env.npm_package_version,
+      appName: pkg.name,
+      version: pkg.version,
     };
   }
 }
